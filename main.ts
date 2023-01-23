c81a828c0db9906b3ee2e5ed38527a99dfe0bbf7
shreya-40005 <122539803+shreya-40005@users.noreply.github.com>
2023-01-23 08:45:14 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,22 +2,61 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("Hello!")
 })
 input.onButtonPressed(Button.AB, function () {
-    PacMan = images.createBigImage(`
-        . # # # . . . . . .
-        # # # . . . . . . .
-        # # . . # . # . # .
-        # # # . . . . . . .
-        . # # # . . . . . .
-        `)
+    PacMan = 0
     for (let index = 0; index < 4; index++) {
-        PacMan.scrollImage(1, 200)
+        images.createBigImage(`
+            . . . . . . # # # .
+            . . . . . . . # # #
+            . # . # . # . . # #
+            . . . . . . . # # #
+            . . . . . . # # # .
+            `).scrollImage(1, 200)
     }
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("Bye!")
 })
 input.onGesture(Gesture.Shake, function () {
+    ball = 0
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
 	
 })
-let PacMan: Image = null
+let ball = 0
+let PacMan = 0
 basic.showIcon(IconNames.Happy)
